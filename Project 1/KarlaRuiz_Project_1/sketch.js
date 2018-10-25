2beee9e3d50a8a61d292090ba9e88f876d663878
Karla Joselly Ruiz <karla.joselly@gmail.com>
2018-10-25 00:21:58 -0400

    Update

--- Project 1/KarlaRuiz_Project_1/sketch.js
+++ Project 1/KarlaRuiz_Project_1/sketch.js
@@ -2,31 +2,41 @@
 // Karla Ruiz
 // Drawing Machine
 
-var bugs = [];
+var circle = [];
+//var line = [];
 
 function setup() {
-  createCanvas(240, 120);
-  for (var i = 0; i < 33; i++) {
+  createCanvas(600, 600);
+  for (var i = 0; i < 45; i++) {
     var x = random(width);
     var y = random(height);
     var r = i + 2;
-    bugs[i] = new JitterBug(x, y, r);
+    circle[i] = new Jitter(x, y, r);
+    //line[i] =  new Jitter(x%20,y%30,i*2);
   }
   background(204);
 }
 
 function draw() {
-  for (var i = 0; i < bugs.length; i++) {
-    bugs[i].move();
-    bugs[i].display();
+  for (var i = 0; i < circle.length; i++) {
+    circle[i].move();
+    circle[i].display();
+      
+//      for (var j = 0; j < line.length; i++) {
+//          line[j].move();
+//          
+//      }
+//      
+      
+      
   }
 }
 
-function JitterBug(tempX, tempY, tempDiameter) {
+function Jitter(tempX, tempY, tempDiameter) {
   this.x = tempX;
   this.y = tempY;
   this.diameter = tempDiameter;
-  this.speed = 2.5;
+  this.speed = 3.5;
   
   this.move = function() {
     this.x += random(-this.speed, this.speed);
@@ -34,6 +44,7 @@ function JitterBug(tempX, tempY, tempDiameter) {
   }
   
   this.display = function() {
+      
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
